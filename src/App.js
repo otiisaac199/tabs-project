d00643f15a6ca049d9daa7e966fc9897b992a399
otiisaac199 <otichinazaisaac19@gmail.com>
2022-10-06 12:10:38 +0100

    chores: added the button

--- src/App.js
+++ src/App.js
@@ -37,7 +37,11 @@ function App() {
       </div>
       <div className="jobs-center">
         {/* btn container */}
-        <div className="btn-container"></div>
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return <button key={item.id}>{item.company}</button>;
+          })}
+        </div>
         {/* job info */}
         <article className="job-info">
           <h3>{title}</h3>
